3c55bb752e3d79c8dde43ec4455417e21ff8ab41
jasonronalddavis <jdavis@c.ringling.edu>
2020-11-12 00:25:08 -0600

    new repository

---
+++ lib/DINO.rb
@@ -0,0 +1,6 @@
+require "DINO/version"
+
+module DINO
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/DINO_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe DINO do
+  it "has a version number" do
+    expect(DINO::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
